002ec8717e383c4f8763e67d469315aaaed2413e
marti <martiespinosa99@gmail.com>
2024-03-16 13:30:51 +0100

    vid 3

--- Map/ViewModels/LocationsViewModel.swift
+++ Map/ViewModels/LocationsViewModel.swift
@@ -6,13 +6,35 @@
 //
 
 import Foundation
+import MapKit
+import SwiftUI
 
 class LocationsViewModel: ObservableObject {
     
+    // All loaded locations
     @Published var locations: [Location]
+    
+    // Current location on map
+    @Published var mapLocation: Location {
+        didSet {
+            updateMapRegion(location: mapLocation)
+        }
+    }
+    
+    @Published var mapRegion: MapCameraPosition = MapCameraPosition.region(MKCoordinateRegion())
+    let mapSpan = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
     
     init() {
         let locations = LocationsDataService.locations
         self.locations = locations
+        self.mapLocation = locations.first!
+        
+        self.updateMapRegion(location: locations.first!)
+    }
+    
+    private func updateMapRegion(location: Location) {
+        withAnimation(.easeInOut) {
+            mapRegion = MapCameraPosition.region(MKCoordinateRegion(center: location.coordinates, span: mapSpan))
+        }
     }
 }
