410372d77529d52295fb9fc169dc460ca7021d63
marti <martiespinosa99@gmail.com>
2024-03-16 12:18:18 +0100

    vid 1

--- Map/Models/Location.swift
+++ Map/Models/Location.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+import MapKit
+
+struct Location {
+    
+    let name: String
+    let cityName: String
+    let coordinates: CLLocationCoordinate2D
+    let description: String
+    let imageNames: [String]
+    let link: String
+    
+}
