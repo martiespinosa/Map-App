2c35148f43df7955e0904e392e5110f68fde41de
marti <martiespinosa99@gmail.com>
2024-03-16 11:47:27 +0100

    Initial Commit

---
+++ Map/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Map
+//
+//  Created by Martí Espinosa Farran on 16/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Map/MapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapApp.swift
+//  Map
+//
+//  Created by Martí Espinosa Farran on 16/3/24.
+//
+
+import SwiftUI
+
+@main
+struct MapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
